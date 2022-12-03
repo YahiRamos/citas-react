768a9df4983c6855526ffcc88ebc986bada8e9ad
YAHIR RAMOS <yahiramoslinks@hotmail.com>
2022-12-02 23:43:42 -0600

    cambios al state de pacientes, para leer data de local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { useState ,useEffect} from 'react'
+import {useState ,useEffect} from 'react'
 import './App.css'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import ListadoPacientes from './components/ListadoPacientes'
 function App() {
 
-  const [pacientes,setPacientes] = useState([])
+  //en el state de los pacientes, carga el local storage
+  //si el local storage está vacio, el state inicia con un arreglo vacio
+  const [pacientes,setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) || [])
   const [paciente,setPaciente]= useState({})
-
-  //obtiene los datos del local storage
-  useEffect(()=>{
-    const obtenerLocalStorage=()=>{
-      //convierte el string almacenado en local strorage a un arreglo
-      let pacientesEnLocalStorage=JSON.parse(localStorage.getItem('pacientes')) ?? []
-      //actualiza el state con la data de local storage
-      setPacientes(pacientesEnLocalStorage)
-    }
-  },[])
 
   //guarda los items en el local storage
   useEffect(()=>{
